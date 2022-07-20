17712bffdd3fc5bb7f432d79cf00ca6adcefff24
Gabriel Almeida de Oliveira <eng.gabriel.ao@gmail.com>
2022-07-19 21:28:34 -0300

    Componente FavoriteRecipes iniciado

--- src/pages/FavoriteRecipes.js
+++ src/pages/FavoriteRecipes.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import DoneFavFilter from '../components/DoneFavFilter';
+import RecipeContext from '../context/RecipeContext';
+import ShareButton from '../components/ShareButton';
 
 function FavoriteRecipes({ history: { location: { pathname } } }) {
+  const { favoriteRecipes, filteredRecipes,
+    setFilteredRecipes } = useContext(RecipeContext);
+
+  useEffect(() => setFilteredRecipes(favoriteRecipes),
+    [favoriteRecipes, setFilteredRecipes]);
+
+  const createRecipeCards = (recipe, index) => (
+    <div key={ recipe.id }>
+      <Link to={ `/${recipe.type}s/${recipe.id}` }>
+        <img
+          src={ recipe.image }
+          alt={ recipe.name }
+          data-testid={ `${index}-horizontal-image` }
+          width="100px"
+          height="100px"
+        />
+      </Link>
+      <ShareButton recipe={ recipe } index={ index } />
+    </div>
+  );
+
   return (
     <div>
       <Header pathname={ pathname } />
-      <span>pagina de FavoriteRecipes</span>
+      <DoneFavFilter pathname={ pathname } />
+      <div>
+        { filteredRecipes.map(createRecipeCards) }
+      </div>
     </div>
   );
 }
