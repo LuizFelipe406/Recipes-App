75f4ca6f618328478fa45a3bd09560e98e6a0945
LuizFelipe406 <luizfpereira406@gmail.com>
2022-08-16 11:35:32 -0300

    Style: Ajusta o height de Recipe Details

--- src/pages/RecipeDetails.js
+++ src/pages/RecipeDetails.js
@@ -209,7 +209,8 @@ function RecipeDetails({ history, match }) {
           <iframe
             width="100%"
             height="315"
-            src={ recipe.strYoutube.replace('watch?v=', 'embed/') }
+            src={ recipe.strYoutube !== undefined
+              ? recipe.strYoutube.replace('watch?v=', 'embed/') : '' }
             title="YouTube video player"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope"
